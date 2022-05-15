9a162ac54b4bf00e871775f506ebb5396d4fd636
nedoff <vovik.ned@gmail.com>
2022-05-15 16:20:11 +0300

    last update

--- app/Http/Controllers/TodolistController.php
+++ app/Http/Controllers/TodolistController.php
@@ -14,77 +14,76 @@ class TodolistController extends Controller
 
     function __construct()
     {
-
-        $this->redirect_url =  route('index');
-
+        $this->redirect_url = route('index');
     }
 
 
     public function index()
     {
-
         $list = Todolist::all();
-        return view('index', ['list' => $list]);
 
+        return view('index', ['list' => $list]);
     }
 
     public function create(StoreTodolistRequest $request)
     {
-
         $task = Todolist::create([
-            'text' => $request->text,
+            'text'   => $request->text,
             'status' => 0,
         ]);
 
-        if(!$task->id){
+        if ( ! $task->id) {
             return redirect($this->redirect_url)->withErrors('Error!');
         }
 
         return redirect($this->redirect_url)->with('success', 'Task added "'.$task->text.'" Sucessful!');
-
     }
 
 
-    public function delete($id, $xxx = 0)
+    public function delete($id)
     {
-
-        $task = Todolist::find($id);
+        $task    = Todolist::find($id);
         $deleted = Todolist::where('id', $id)->delete();
 
-        if($deleted == 1){
+        if ($deleted == 1) {
             return redirect($this->redirect_url)->with('success', 'Task "'.$task->text.'" removed Sucessful!');
         }
 
         return redirect($this->redirect_url)->withErrors('Error!');
-
     }
 
 
-    public function done($id)
+    public function done($id = null)
     {
+        if ( ! $id) {
+            return abort(404);
+        }
 
         $done = Todolist::where('id', $id)->update(['status' => 1]);
         $task = Todolist::find($id);
-        if($done == 1){
+
+        if ($done == 1) {
             return redirect($this->redirect_url)->with('success', 'Task "'.$task->text.'" done Successful!');
         }
 
         return redirect($this->redirect_url)->withErrors('Done Error!');
-
     }
 
 
-    public function undone($id)
+    public function undone($id = null)
     {
-
-        $undone = Todolist::where('id', $id)->update(['status' => 0]);
-        $task = Todolist::find($id);
-        if($undone == 1){
-            return redirect(route('index'))->with('success', 'Task "'.$task->text.'" undone Successful!');
+        if ( ! $id) {
+            return abort(404);
         }
 
-        return redirect(route('index'))->withErrors('Undone Error!');
+        $undone = Todolist::where('id', $id)->update(['status' => 0]);
+        $task   = Todolist::find($id);
 
+        if ($undone == 1) {
+            return redirect($this->redirect_url)->with('success', 'Task "'.$task->text.'" undone Successful!');
+        }
+
+        return redirect($this->redirect_url)->withErrors('Undone Error!');
     }
 
 }
